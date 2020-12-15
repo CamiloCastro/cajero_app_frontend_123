46edd765080ee5fe87b12006f230cbc6b2e75d02
Camilo <jccastrop@unal.edu.co>
2020-12-15 08:26:46 -0500

    add table transactions

--- src/components/Transaction.vue
+++ src/components/Transaction.vue
@@ -1,18 +1,41 @@
 <template>
   <div id="Transaction">
-    
+    <b-container>
       <b-row>
         <b-col cols="4">Cantidad:</b-col>
         <b-col cols="8"><b-form-input style="width: 50%" type="text" placeholder="Cantidad" v-model="cantidad" /></b-col>
       </b-row>
       <b-row>
+        <b-col>
           <b-button variant="dark" v-on:click="makeTransaction">Hacer transacción</b-button>
+        </b-col>
       </b-row>
-    
+      <br />
+
+      <b-row>
+        <table>
+          <tr>
+            <th>Id de la transacción</th>
+            <th>Fecha de la transacción</th>
+            <th>Valor de la transacción</th>
+          </tr>
+
+          <tr v-for="t in transactions" v-bind:key="t.id_transaction">
+            <td>{{ t.id_transaction }}</td>
+            <td>{{ t.date }}</td>
+            <td>{{ t.value }}</td>
+          </tr>
+
+        </table>
+      </b-row>
+
+      <div>
+        <b-table striped hover :items="transactions"></b-table>
+      </div>
 
 
-    
-    
+
+    </b-container>
   </div>
 </template>
 
@@ -21,6 +44,12 @@ import axios from "axios";
 
 export default {
   name: "Transaction",
+  data: function () {
+    return {
+      cantidad: "",
+      transactions: []
+    };
+  },
   methods: {
     makeTransaction: function() {
 
@@ -38,6 +67,25 @@ export default {
         alert("ERROR Servidor");
       });
     }
+  },
+  created: function() {
+    var current_username = localStorage.getItem("current_username");
+    let self = this;
+    axios.get("http://localhost:8000/user/transactions/" + current_username)
+      .then((result) => {
+        self.transactions = result.data;
+      })
+      .catch((error) => {
+        alert("ERROR Servidor");
+      });
   }    
 }
 </script>
+
+<style>
+
+td {
+  text-align: center;
+}
+
+</style>
--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,8 +20,7 @@ export default {
   created: function () {
     this.username = this.$route.params.username;
     let self = this;
-    axios
-      .get("http://localhost:8000/user/balance/" + this.username)
+    axios.get("http://localhost:8000/user/balance/" + this.username)
       .then((result) => {
         self.balance = result.data.balance;
       })
